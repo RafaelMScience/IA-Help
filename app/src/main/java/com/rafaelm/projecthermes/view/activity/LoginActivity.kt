4e814c1958d9d4943e4accc84cd64c8e52468916
RafaelM <rafaelm.science@gmail.com>
2020-09-23 13:16:15 -0400

    Update: Create Login

---
+++ app/src/main/java/com/rafaelm/projecthermes/view/activity/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.rafaelm.projecthermes.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
+import com.rafaelm.projecthermes.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun singUp(view: View) {
+
+    }
+}
